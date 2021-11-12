377b93cebc9e443ff85abdb137cc644d86f166b3
asgersong <asger.poulsen@yahoo.com>
2021-11-12 09:15:04 +0100

    Implemented functions and changed SENTINEL to NULL

--- src/linked_list.c
+++ src/linked_list.c
@@ -13,25 +13,25 @@ node *make_node (int v, node * q)
   node *p = malloc (sizeof (node));
   p->value = v;
 
-  if (q == &SENTINEL_node)
-    p->next = &SENTINEL_node;
+  if (q == NULL)
+    p->next = NULL;
   else
     p->next = q;
   return p;
 }
 
 /* free all nodes in the list p */
-void free_list (node * p)
+node *free_list (node * p) // changed function signature from "void" to "node *"
 {
   node *q = p;
-  while (q != &SENTINEL_node)
+  while (q != NULL)
     {
       node *t = q->next;
       free (q);
       q = t;
     }
+  return q;
 }
-
 
 /* print list to console */
 void print_list (node * p)
@@ -39,8 +39,9 @@ void print_list (node * p)
     // Add your code for exercise 1
     // There is NO testcode for this
 
-    if (p == NULL) // simple case #1
-      return;
+    if (p == NULL) // simple case #1 for when end of linked-list is set to point to NULL
+      printf("\n");
+
     else
     {
       printf("%d ", p -> value); // first print the value of the first node in linked list
@@ -53,14 +54,25 @@ int sum_squares (node * p)
 {
     // Add your code for excercise 2
     // You can find the tests in tests.cpp
-    return -1;
+    if (p == NULL) // simple case #1
+      return 0;
+
+    if (p -> next == NULL) // simple case #2
+      return square(p -> value);
+
+    else 
+      return square(p -> value) + sum_squares(p -> next); // recursive step
 }
 
 node *map (node * p, int (*f) (int))
 {
     // Add your code for excercise 3
-    return NULL;
-}
+    if (p == NULL) // simple case
+      return NULL;
+    // else
+      return (make_node((*f)(p -> value), map(p -> next, f))); // recursive step
+    
+  }
 
 
 int square (int x)
@@ -72,9 +84,9 @@ int square (int x)
 // inserts node at the end of linked list
 node *insert (node * p, int val)
 {
-  node * temp = (struct node*)malloc(sizeof(struct node));
+  node * temp = malloc (sizeof (node));
   temp -> value = val;
-  temp -> next = NULL;
+  temp -> next = NULL; // make sure that end of list still is NULL
   if (p == NULL) // if list is empty
     p = temp;
   else
@@ -84,5 +96,6 @@ node *insert (node * p, int val)
       temp1 = temp1 -> next;
     temp1 -> next = temp;
   }
+  
 return p;
 }
